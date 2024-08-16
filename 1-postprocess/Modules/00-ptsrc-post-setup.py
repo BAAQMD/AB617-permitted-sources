c4bc9de114e45c82902174c7e7ac5d98eda44f8a
nwtang <nick.w.tang@gmail.com>
2024-08-16 13:48:22 -0700

    Add postprocessing modules

---
+++ 1-postprocess/Modules/00-ptsrc-post-setup.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+import glob
+
+# Path to AERMOD outputs
+aermod_path = "Input/aermod"
+
+# Path to permitted sources setup file
+ps_path = "Input/Setup_PermittedSources_EastOakland.xlsx"
+
+# Path to cancer slope factors
+cancslpf_path = "Input/cancslpf.csv"
